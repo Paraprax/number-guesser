eb340f8e9199a74dd6bb0d793e90b030cbe1058d
Paraprax <26296581+Paraprax@users.noreply.github.com>
2022-05-01 16:20:51 -0400

    Write guess-comparing function

--- script.js
+++ script.js
@@ -6,3 +6,34 @@ let currentRoundNumber = 1;
 const generateTarget = () => {
   return Math.floor(Math.random() * 10);
 };
+
+const compareGuesses = (userNum, compNum, targetNum) => {
+  let userDiff;
+  let compDiff;
+  let outcome;
+
+  //user result:
+  if (userNum >= targetNum) {
+    userDiff = userNum - targetNum;
+  } else if (targetNum > userNum) {
+    userDiff = targetNum - userNum;
+  }
+
+  //computer result:
+  if (compNum >= targetNum) {
+    compDiff = compNum - targetNum;
+  } else if (targetNum > compNum) {
+    compDiff = targetNum - compNum;
+  }
+
+  //calculate outcome:
+  if (userDiff <= compDiff) {
+    outcome = true;
+  } else {
+    outcome = false;
+  }
+
+  return outcome;
+};
+
+console.log(compareGuesses(7, 5, 8));
